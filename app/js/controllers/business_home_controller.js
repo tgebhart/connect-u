ec5c0441866351f177e32aeb7f738be9cb4cd5a7
tgebhart <gebharttom8@gmail.com>
2015-09-11 15:38:19 -0500

    profile dropdown

--- app/js/controllers/business_home_controller.js
+++ app/js/controllers/business_home_controller.js
@@ -1,10 +1,17 @@
 angular.module("app").controller('BusinessHomeController', function($scope, $location, AuthenticationService, $log) {
   $scope.title = "Home";
   $scope.message = "Stuff";
+  $scope.profileDropdownState = false;
 
   var onLogoutSuccess = function(response) {
     $location.path('/login');
   };
+
+  $scope.isToggled = function(){
+    console.log($scope.profileDropdownState);
+    $scope.profileDropdownState = !$scope.profileDropdownState;
+  };
+
 
   $scope.logout = function() {
     AuthenticationService.logout().success(onLogoutSuccess);
